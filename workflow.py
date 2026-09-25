96c4b5aa3807f5242c7a4f70892af55c59a99ab0
Cláudio Diego Teixeira de Souza - PrestServ <g86s@gaiasc03.ep.petrobras.com.br>
2026-09-25 11:40:24 -0300

    cross validation

--- workflow.py
+++ workflow.py
@@ -9,18 +9,19 @@ print('Workflow start', datetime.now().strftime("%d/%m/%Y - %H:%M:%S"))
 
 torch.manual_seed(42)
 
-arr = np.loadtxt('/home/src/experiments.csv', delimiter=',', dtype=str)
+arr = np.loadtxt('/home/src/experiments_new_data.csv', delimiter=',', dtype=str)
 loss_paths = []
 
 for line in arr:
     is_supervised =  True if line[0] == 'True' else False
     train_y_path = line[1]
-    epochs = int(line[2])
-    lr = float(line[3])
-    base_channels = int(line[4])
-    test_y_path = line[5]
+    n_folds = int(line[2])
+    epochs = int(line[3])
+    lr = float(line[4])
+    base_channels = int(line[5])
+    test_y_path = line[6]
 
-    result_dir = call_train(is_supervised, train_y_path, epochs, lr, base_channels)
+    result_dir = call_train(is_supervised, train_y_path, n_folds, epochs, lr, base_channels)
     loss_paths.append(f'{result_dir}/loss_history.npy')
 
     if train_y_path != test_y_path:
